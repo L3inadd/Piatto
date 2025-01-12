d843e163cd2283a0685ed3edd1df2fd59f2a2d62
Ryo <Ryo@pwned.net>
2025-01-12 20:41:05 +0100

    frist part of the code and frist commit

---
+++ src/Papa.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Papa extends Thread {
+    private Piatto piatto;
+
+    public Papa(Piatto piatto){
+        this.piatto = piatto;
+    }
+
+    @Override
+    public void run{
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/Piatto.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+
+public class Piatto {
+   private ArrayList<Integer>  listaBocconi = new ArrayList<Integer>();
+
+    public synchronized void produceBoccone(){
+        Integer i= 1;
+        try {
+            while(listaBocconi.size()>9) {
+                wait();
+                System.out.println("il piatto è pieno per produrre altri bocconi");
+            }
+        }
+        catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            listaBocconi.add(i);
+            notifyAll();
+            System.out.println("Un boccone è stato inserito nel piatto: " +listaBocconi.size() + " all'interno del piatto ");
+        }
+    }
+
+
+    public synchronized void consumaBoccone(){
+        try{
+            while(listaBocconi.isEmpty() ){
+                wait();
+                System.out.println("Il piatto è vuoto :<");
+            }
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        finally{
+            listaBocconi.remove(listaBocconi.size()-1);
+            notifyAll();
+            System.out.println("Sto prendneo un boccone dal piatto");
+        }
+    }
+
+
+}
